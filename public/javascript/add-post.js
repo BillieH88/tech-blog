91245dd53fd960ebc075b95342e579d5a4959827
Billie  Hussein <billiehussein@Billies-MacBook-Pro.local>
2021-07-21 03:36:50 -0400

    fix error in create post

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -1,24 +1,28 @@
 async function newFormHandler(event) {
-  event.preventDefault();
+  try {
+    event.preventDefault();
 
-  const title = document.querySelector('input[name="post-title"]').value;
-  const content = document.querySelector('input[name="post-content"]').value;
-
-  const response = await fetch(`/api/posts`, {
-    method: 'POST',
-    body: JSON.stringify({
-      title,
-      content
-    }),
-    headers: {
-      'Content-Type': 'application/json'
+    const title = document.querySelector('input[name="post-title"]').value;
+    const content = document.querySelector('input[name="post-content"]').value;
+  
+    const response = await fetch(`/api/posts`, {
+      method: 'POST',
+      body: JSON.stringify({
+        title,
+        content
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+  
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert(response.statusText);
     }
-  });
-
-  if (response.ok) {
-    document.location.replace('/dashboard');
-  } else {
-    alert(response.statusText);
+  } catch (err) {
+    console.log('an error occurred==>>', err);
   }
 }
 
